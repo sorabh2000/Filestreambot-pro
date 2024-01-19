5b262c9de375697976b170c5f18e872f14b83ed0
sorabh2000 <146937450+sorabh2000@users.noreply.github.com>
2024-01-19 08:46:05 +0530

    vars.py

--- Adarsh/vars.py
+++ Adarsh/vars.py
@@ -9,13 +9,13 @@ load_dotenv()
 
 class Var(object):
     MULTI_CLIENT = False
-    API_ID = int(getenv('API_ID'))
-    API_HASH = str(getenv('API_HASH'))
-    BOT_TOKEN = str(getenv('BOT_TOKEN'))
+    API_ID = int(getenv('API_ID', '21691588'))
+    API_HASH = str(getenv('API_HASH', '494845d6ac932abff6e830d28e5a3037'))
+    BOT_TOKEN = str(getenv('BOT_TOKEN', '6962863338:AAEe5UUmJz3pG1BYdZdgPop5XHefv9e_BCg'))
     name = str(getenv('name', 'filetolinkbot'))
     SLEEP_THRESHOLD = int(getenv('SLEEP_THRESHOLD', '60'))
     WORKERS = int(getenv('WORKERS', '4'))
-    BIN_CHANNEL = int(getenv('BIN_CHANNEL'))
+    BIN_CHANNEL = int(getenv('BIN_CHANNEL', '-1002032481689'))
     PORT = int(getenv('PORT', 8080))
     BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
     PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))  # 20 minutes
